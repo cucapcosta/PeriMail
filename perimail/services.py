b7177327253ead714ef1099c4d993b50ec84f443
cucapcosta <cucapcosta@gmail.com>
2026-06-09 18:57:43 -0300

    feat: add gmail_service_for_account helper

---
+++ perimail/services.py
@@ -0,0 +1,8 @@
+from perimail.auth import get_credentials, get_gmail_service
+from perimail.crypto import decrypt
+
+
+def gmail_service_for_account(account, encryption_key: bytes):
+    """Build a Gmail API service for a stored account by decrypting its tokens."""
+    credentials = get_credentials(decrypt(account.encrypted_tokens, encryption_key))
+    return get_gmail_service(credentials)
